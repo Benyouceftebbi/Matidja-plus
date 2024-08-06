10ca2baf0b959a7ae0c6bbba239aaf665cdf4e7d
Salah <105821405+SalahDvp@users.noreply.github.com>
2024-08-06 21:29:09 +0100

    Add validators to the student form and edit form

--- app/[locale]/(home)/students/components/studentForm.tsx
+++ app/[locale]/(home)/students/components/studentForm.tsx
@@ -10,6 +10,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useToast } from "@/components/ui/use-toast"
 import { Input } from "@/components/ui/input"
 import {
@@ -74,7 +75,6 @@ interface FooterProps {
 
 const subjects =['متوسط','علوم تجريبية', 'تقني رياضي', 'رياضيات', 'تسيير واقتصاد ', 'لغات اجنبية ', 'اداب وفلسفة']
 const classess = [
- "Select Option",
   "رياضيات",
   "علوم",
   "فيزياء",
@@ -96,13 +96,12 @@ const classess = [
   "تاريخ وجغرافيا",
 
 ];
-const steps = [
+const steps: StepItem[] = [
   { label: "Step 1" },
   { label: "Step 2" },
   { label: "Step 3" },
-  { label: "Step 4" },
+];
 
-] satisfies StepItem[]
 const years=[
   "1AM",
   "2AM",
@@ -128,8 +127,8 @@ export default function StudentForm() {
   const camera = useRef<null | { takePhoto: () => string }>(null);
   const {setStudents,teachers,classes,students}=useData()
   const t=useTranslations()
-  const form = useForm<any>({
-    // resolver: zodResolver(StudentSchema),
+  const form = useForm<Student>({
+    resolver: zodResolver(StudentSchema),
     defaultValues:{
       id:null,
       studentIndex: students.length + 1,
@@ -830,33 +829,4 @@ const Footer: React.FC<FooterProps> = ({ formData, form, isSubmitting,reset}) =>
       </div>
     </>
   )
-}
-function QrCodeIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="5" height="5" x="3" y="3" rx="1" />
-      <rect width="5" height="5" x="16" y="3" rx="1" />
-      <rect width="5" height="5" x="3" y="16" rx="1" />
-      <path d="M21 16h-3a2 2 0 0 0-2 2v3" />
-      <path d="M21 21v.01" />
-      <path d="M12 7v3a2 2 0 0 1-2 2H7" />
-      <path d="M3 12h.01" />
-      <path d="M12 3h.01" />
-      <path d="M12 16v.01" />
-      <path d="M16 12h1" />
-      <path d="M21 12v.01" />
-      <path d="M12 21v-1" />
-    </svg>
-  );
 }
